2d9df621533078ca0c0c4717f81c85990921d2d3
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2026-08-31 13:17:12 +0530

    Day-1

---
+++ Test.py
@@ -0,0 +1,10 @@
+print("welcome to india")
+#this below code will connect  to db
+age=int(input("Enter your age:"))
+if age<18:
+    print("invalid age not allowed to vote")
+else :
+    print("you are allowed to vote ")
+print("outside IF")    
+
+
